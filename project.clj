3d5b40e2bc4391742ec1eed1757a3d3fbc0cc768
Fabian Aussems <fabianaussems@gmail.com>
2017-03-17 18:26:21 +0100

    move re-native to re_native

--- project.clj
+++ project.clj
@@ -1,1 +1,6 @@
-(defproject re-native/navigation "1.0.0-beta.7")
+(defproject re-native/navigation "1.0.0-beta.7"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.473"]
+                 [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
+                 [re-frame "0.9.1"]]
+  :plugins [[lein-cljsbuild "1.1.4"]])
